15b6357084feff0c033679d62b5e4a0a59a2aea3
Anuar Abdullahi <universe.city.trd@gmail.com>
2020-10-05 20:16:23 +0200

    Created animation for the header-section.

--- src/components/organisms/header-section/header-section.component.jsx
+++ src/components/organisms/header-section/header-section.component.jsx
@@ -20,14 +20,37 @@ const HeaderSection = () => {
 				/>
 			</div>
 			<div className='header-text-wrapper'>
-				<motion.h1 className='header-text-top' animate={{}}>
+				<motion.h1
+					className='header-text-top'
+					initial={{ y: '5vh', opacity: 0 }}
+					animate={{ y: 0, opacity: 1 }}
+					transition={{ duration: 0.7 }}
+				>
 					Haar Specialst
 				</motion.h1>
-				<div className='header-divider'></div>
-				<h2 className='header-text-bottom'>Voor alle haar type</h2>
-				<ButtonContained className='button'>
-					Afspraak Maken
-				</ButtonContained>
+				<motion.div
+					className='header-divider'
+					initial={{ width: 0 }}
+					animate={{ width: 200 }}
+					transition={{ delay: 1, duration: 0.7 }}
+				></motion.div>
+				<motion.h2
+					className='header-text-bottom'
+					initial={{ y: '-5vh', opacity: 0 }}
+					animate={{ y: 0, opacity: 1 }}
+					transition={{ delay: 0.3, duration: 0.7 }}
+				>
+					Voor alle haar type
+				</motion.h2>
+				<motion.div
+					initial={{ y: '5vh', opacity: 0 }}
+					animate={{ y: 0, opacity: 1 }}
+					transition={{ delay: 1, duration: 0.7 }}
+				>
+					<ButtonContained className='button'>
+						Afspraak Maken
+					</ButtonContained>
+				</motion.div>
 			</div>
 		</Header>
 	);
