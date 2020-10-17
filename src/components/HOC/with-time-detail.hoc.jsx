8ad37597b1dfb0396ee3fcaf30eb96918145bc5c
Anuar Abdullahi <universe.city.trd@gmail.com>
2020-10-18 00:36:14 +0200

    Created with-time-detail HOC.

---
+++ src/components/HOC/with-time-detail.hoc.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { createStructuredSelector } from 'reselect';
+import { connect } from 'react-redux';
+
+// Redux: Selectors
+import {
+	selectIsEdit,
+	selectTimeDetail,
+} from '../../redux/handlers/time-detail/time-detail.selectors';
+
+// Redux: Actions
+import {
+	fetchTimeStart,
+	toggleEditStart,
+	clearTime,
+} from '../../redux/handlers/time-detail/time-detail.actions';
+
+const WithTimeDetail = WrappedComponent => {
+	const WithData = props => {
+		return <WrappedComponent {...props} />;
+	};
+
+	// Redux: Selectors
+	const mapStateToProps = createStructuredSelector({
+		timeDetail: selectTimeDetail,
+		isEdit: selectIsEdit,
+	});
+
+	// Redux: Actions
+	const mapDispatchToProps = dispatch => ({
+		fetchTimeStart: () => dispatch(fetchTimeStart()),
+		toggleEditStart: () => dispatch(toggleEditStart()),
+		clearTime: () => dispatch(clearTime()),
+	});
+
+	return connect(mapStateToProps, mapDispatchToProps)(WithData);
+};
+
+export default WithTimeDetail;
